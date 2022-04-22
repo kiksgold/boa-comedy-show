5350dfd3afefe8798327c5b293ffb86766e61ff8
kiksgold <kiksgold@gmail.com>
2022-04-22 16:40:31 +0000

    Create a function to get the ticket numbers from the users

--- run.py
+++ run.py
@@ -12,10 +12,18 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('boa-comedy-show')
 
-sales = SHEET.worksheet('ticket_sales')
+def get_ticket_num():
+    """
+    Get ticket numbers in figure input from the user
+    """
+    print("Please enter the ticket numbers from the last event.")
+    print("Data should be three numbers, separated by commas.")
+    print("Example: 60,80,30\n")
 
-numbers = sales.get_all_values()
+    num_str = input("Enter your numbers here: ")
+    print(f"The numbers provided are {num_str}")
 
-print(numbers)
+get_ticket_num()
+
 
 
