257ee671d440dd4601ba416e6ce51543a2c8ac7f
kiksgold <kiksgold@gmail.com>
2022-04-24 21:39:07 +0000

    Define function for unsold tickets and get latest stock numbers

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -61,6 +62,18 @@ def update_ticket_worksheet(data):
     print('Ticket worksheet updated successfully.\n')
 
 
+def calculate_unsold_data(ticket_row):
+    """
+    We will compare the ticket sales with the inventory in order to calculate the unsold(if any) of the categories
+    """
+    print('Calculating unsold tickets...\n')
+    inventory = SHEET.worksheet('inventory').get_all_values()
+    inventory_row = inventory[-1]
+    print(inventory_row)
+
+
+
+
 def master():
     """
     Run all our functions here
@@ -68,6 +81,7 @@ def master():
     data = get_ticket_data()
     ticket_data = [int(fig) for fig in data]
     update_ticket_worksheet(ticket_data)
+    calculate_unsold_data(ticket_data)
 
 
 print('Welcome to BOA Comedy Show Automation')
